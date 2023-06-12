a2327f317c4681a15b56bbff17e9d02dab0cc8c5
TomKypils <guilhermeff@hotmail.com>
2023-06-12 13:54:37 -0400

    Initial change of Budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,45 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, dispatch } = useContext(AppContext);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleIncrease = () => {
+    const newValue = parseInt(inputValue) + 10;
+
+    dispatch({
+        type: 'SET_BUDGET',
+        payload: newValue,
+    });
+    setInputValue('');
+  };
+
+  const handleDecrease = () => {
+    const newValue = parseInt(inputValue) - 10;
+
+    dispatch({
+        type: 'SET_BUDGET',
+        payload: newValue,
+    });
+    setInputValue('');
+  };
+
+  return (
+    <div className='alert alert-secondary'>
+      <span>Budget: £{budget}</span>
+      <div>
+        <input type="number" step="10" value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleIncrease}>Increase</button>
+        <button onClick={handleDecrease}>Decrease</button>
+      </div>
+    </div>
+  );
 };
+
 export default Budget;
+
