f464ebd1ba0da8eb653735d048e2a3583e18e447
TomKypils <guilhermeff@hotmail.com>
2023-06-14 10:18:53 -0400

    Changed increase icon and added decrease icon

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai'
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -25,12 +26,27 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><AiFillPlusCircle size='2.2em' color="green" onClick={event=> increaseAllocation(props.name)}></AiFillPlusCircle></td>
+        <td><AiFillMinusCircle size='2.2em' color="red" onClick={event=> decreaseAllocation(props.name)}></AiFillMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        
         </tr>
     );
 };
